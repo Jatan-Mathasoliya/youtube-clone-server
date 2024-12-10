4b4f5cfa934bb01eba70b16f0962366c53a56932
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2024-12-10 15:28:27 +0530

    This is a backend code for my youtube clone that i make using ReactJS and Nodejs

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use('/assets', express.static('./public/assets'));
+
+const data = [
+    {id:1, thumb:'/assets/t8.png', profile:'/assets/1.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:2, thumb:'/assets/t9.png', profile:'/assets/2.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t10.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t11.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t4.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t5.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t6.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t7.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t3.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t3.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t3.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+    {id:3, thumb:'/assets/t3.png', profile:'/assets/3.png', title:"Bulbuli|coke studio Bangla|season one|Ritu Raj X Nandita", name:"Coke studio Bangla", img:'/assets/verified.png', text:"1.5M views  2 days ago"},
+]
+
+app.get('/videos', (req, res)=>{
+    res.json(data)
+})
+
+app.listen(port, ()=>{
+    console.log(`server running on ${port}`)
+})
